ab7b951033ca7c4dbb873e24028cbe387f94f9cd
Sweet Van Loan <Sweet@SweetVanLoan.com>
2020-09-21 17:48:56 -0500

    toy assoc. started

--- main_app/models.py
+++ main_app/models.py
@@ -11,18 +11,6 @@ MEALS = (
 )
 
 # Create your models here.
-class Dog(models.Model):
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    age = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'dog_id': self.id})
-
 class Toy(models.Model):
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=20)
@@ -32,6 +20,23 @@ class Toy(models.Model):
 
     def __str__(self):
         return f"A {self.color} {self.name}"
+        
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
 
 class Feeding(models.Model):
     date = models.DateField('Feeding Date')
@@ -45,3 +50,6 @@ class Feeding(models.Model):
 
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+         ordering = ['-date']
